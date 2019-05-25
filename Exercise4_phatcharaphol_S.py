37dc7df4f06060bf651e7f1040992c27dd97b7e1
Ohmmagosh <48948274+Ohmmagosh@users.noreply.github.com>
2019-05-25 19:57:41 +0700

    Create Exercise4_phatcharaphol_S.py

---
+++ Exercise4_phatcharaphol_S.py
@@ -0,0 +1,9 @@
+FoundationEnglish= 80
+GeneralBusiness =73.96
+IntroductiontoComputerSystems =54
+ComputerProgramming =67.59
+print("[---------------------YOU SCORE------------------------]")
+print("  Foundation English                           :",FoundationEnglish)
+print("  General Business                             :",GeneralBusiness)
+print("  Introduction to Computer Systems             :",IntroductiontoComputerSystems)
+print("  Computer Programming                         :",ComputerProgramming)
